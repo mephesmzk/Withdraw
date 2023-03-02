098c2ea7b995010fbaca4232882a3b685d9e0c0b
Luciano Manzke Lindemann <lucianomanzke@gmail.com>
2023-03-02 17:27:31 -0300

    base version

---
+++ src/application/Program.java
@@ -0,0 +1,33 @@
+package application;
+
+import entities.Account;
+
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter account data");
+        System.out.print("Number: ");
+        int number = sc.nextInt();
+        System.out.print("Holder: ");
+        sc.nextLine();
+        String holder = sc.nextLine();
+        System.out.print("Initial balance: ");
+        double balance = sc.nextDouble();
+        System.out.print("Withdraw limit: ");
+        double withdrawLimit = sc.nextDouble();
+
+        Account acc = new Account(number, holder, balance, withdrawLimit);
+
+        System.out.println();
+        System.out.print("Enter amount for withdraw: ");
+        double amount = sc.nextDouble();
+
+        acc.withdraw(amount);
+        System.out.printf("New balance: %.2f\n", acc.getBalance());
+    }
+}
