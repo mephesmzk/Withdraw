ae2dc57a300a6920ded67fad4549bafc38edbe7a
Luciano Manzke Lindemann <lucianomanzke@gmail.com>
2023-03-03 13:52:04 -0300

    Solution with exceptions

--- src/entities/Account.java
+++ src/entities/Account.java
@@ -1,5 +1,7 @@
 package entities;
 
+import exceptions.BusinessException;
+
 public class Account {
     private Integer number;
     private String holder;
@@ -53,16 +55,16 @@ public class Account {
     }
 
     public void withdraw(double amount) {
+        validateWithdraw(amount);
         balance -= amount;
     }
 
-    public String validateWithdraw (double amount) {
+    private void validateWithdraw (double amount) {
         if (amount > getWithdrawLimit()) {
-            return "Withdraw error: The amount exceeds withdraw limit";
+            throw new BusinessException("Withdraw error: The amount exceeds withdraw limit");
         }
         if (amount > getBalance()) {
-            return "Withdraw error: Not enough balance";
+            throw new BusinessException("Withdraw error: Not enough balance");
         }
-        return null;
     }
 }
